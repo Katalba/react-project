efcffa18f639c9ee684d3e1bc3633bb6e81f46d0
katalba <katherine.albanicg@gmail.com>
2023-03-02 20:56:16 -0300

    checkoutForm-done

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -1,41 +1,50 @@
 import { useCartContext } from "../Context/CartContext";
 import { Link } from "react-router-dom";
-import React from "react";
 import { useNavigate } from "react-router-dom";
-import { createPurchaseOrder, getPurchaseOrder, getProduct, updateProduct } from "../../utils/utils";
+import { createPurchaseOrder, getProduct, updateProduct } from "../../utils/utils";
+import { useForm } from 'react-hook-form'
+import React from "react";
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
-import {useForm} from 'react-hook-form'
+
+
 
 export const Checkout = () => {
     const { cart, emptyCart, totalPrice } = useCartContext()
     const datosFormulario = React.useRef()
-    const {register, handleSubmit, formState: {errors}} = useForm()
+    const { register, handleSubmit, formState: { errors }, getValues } = useForm()
+
+
+
     const onSubmit = (data) => {
         console.log(data)
-    }
-
-    let navigate = useNavigate()
-
-    const consultarFormulario = (e) => {
-        e.preventDefault()
         const formDat = new FormData(datosFormulario.current)
         const client = Object.fromEntries(formDat)
 
         const aux = [...cart]
 
+        aux.forEach(prodCart => {
+            getProduct(prodCart.id).then(prodBDD => {
+                prodBDD.stock -= prodCart.cant
+                updateProduct(prodCart.id, prodBDD)
+            })
+        })
+
         createPurchaseOrder(client, aux, totalPrice(), new Date().toISOString()).then(purchaseOrder => {
             const MySwal = withReactContent(Swal)
             MySwal.fire({
                 title: 'Gracias por su compra',
-                text: `Su orden de compra por $ ${new Intl.NumberFormat('de-DE').format(totalPrice())} fue realizada con éxito`,
+                text: `Su orden de compra: ${purchaseOrder.id} por: $${new Intl.NumberFormat('de-DE').format(totalPrice())} fue realizada con éxito`,
                 icon: 'success',
             })
             emptyCart()
-            e.target.reset()
+            data.target.reset()
             navigate('/')
         })
     }
+
+    let navigate = useNavigate()
+
     return (
         <>
             {cart.length === 0
@@ -52,27 +61,57 @@ export const Checkout = () => {
                 <form onSubmit={handleSubmit(onSubmit)} ref={datosFormulario} >
                     <div className="mb-3">
                         <label className="form-label">Nombre y Apellido</label>
-                        <input {...register('nameLastname', {required:true} )} type="name" placeholder="Juan Castro" className="form-control" aria-describedby="name-lastName" />
+                        <input {...register('nameLastname', { required: true })} type="name" placeholder="Juan Castro" className="form-control" aria-describedby="name-lastName" />
                         {errors.nameLastname && <small className="fail">Campo requerido</small>}
                     </div>
                     <div className="mb-3">
-                        <label className="form-label">E-mail</label>
-                        <input {...register('email')} type="email" placeholder="Introduzca su email" className="form-control" />
-                        {errors.nameLastname && <small className="fail">Campo requerido</small>}
+                        <label className="form-label">Email</label>
+                        <input
+                            className="form-control"
+                            type="email"
+                            name="email1"
+                            {...register("email1", { required: true })}
+                        />
+                        {errors?.email1?.type === "minLength" && (
+                            <small className="fail">La dirección de email no es valida</small>
+                        )}
+                        {errors?.email1?.type === "required" && (
+                            <small className="fail">Es necesario ingresar tu email</small>
+                        )}
                     </div>
                     <div className="mb-3">
-                        <label className="form-label">Repetir E-mail</label>
-                        <input {...register('repeatEmail')} type="email" placeholder="Repita su email" className="form-control" />
-                        {errors.nameLastname && <small className="fail">Campo requerido</small>}
+                        <label className="form-label">Confirmar email</label>
+                        <input
+                            className="form-control"
+                            type="email"
+                            name="email2"
+                            {...register("email2", { required: true, validate: { equalMails: mail2 => mail2 === getValues().email1 } })}
+                        />
+                        {errors?.email2?.type === "minLength" && (
+                            <small className="fail">La dirección de email no es valida</small>
+                        )}
+
+                        {errors?.email2?.type === "required" && (
+                            <small className="fail">Es necesario ingresar tu email</small>
+                        )}
+                        {errors?.email2?.type === "equalMails" && (
+                            <small className="fail">Los emails deben ser iguales</small>
+                        )}
                     </div>
                     <div className="mb-3">
                         <label className="form-label">Número de teléfono</label>
-                        <input {...register('phoneNumber', {required:true} )} type="number" placeholder="1112345678" className="form-control" />
+                        <input {...register('phoneNumber', { required: true })}
+                            type="number"
+                            placeholder="1112345678"
+                            className="form-control" />
                         {errors.nameLastname && <small className="fail">Campo requerido</small>}
                     </div>
                     <div className="mb-3">
                         <label className="form-label">Dirección de envío</label>
-                        <input {...register('address')} type="text" placeholder="Hidalgo 765 1 E" className="form-control" />
+                        <input {...register('address')}
+                            type="text"
+                            placeholder="Hidalgo 765 1 E"
+                            className="form-control" />
                         {errors.nameLastname && <small className="fail">Campo requerido</small>}
                     </div>
                     <button type="submit" className="btn btn-dark submit">Finalizar compra</button>
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import {useForm} from 'react-hook-form'
 import { toast } from 'react-toastify';
 
 const Contact = () => {
   const datosFormulario = React.useRef()
-  const {register, handleSubmit} = useForm()
   let navigate = useNavigate()
   const consultarFormulario = (e) => {
     e.preventDefault()
@@ -24,18 +22,18 @@ const Contact = () => {
             <img src="https://firebasestorage.googleapis.com/v0/b/valkiria-react.appspot.com/o/LOGO.png?alt=media&token=0d99007e-f9e1-46fb-8c4e-dcd8a78c2e31" alt="logo" width="300px" />
           </Link>
         </div>
-        <form onSubmit={handleSubmit} ref={datosFormulario}>
+        <form onSubmit={consultarFormulario} ref={datosFormulario}>
           <div className="mb-3">
             <label className="form-label">Nombre y Apellido</label>
-            <input {...register('name-Lastname')} type="name" className="form-control" aria-describedby="name-lastName" />
+            <input name='name-Lastname' type="name" className="form-control" aria-describedby="name-lastName" />
           </div>
           <div className="mb-3">
             <label className="form-label">E-mail</label>
-            <input {...register('email')} type="email" className="form-control" />
+            <input name='email' type="email" className="form-control" />
           </div>
           <div className="mb-3">
             <label className="form-label">¿Cómo podemos ayudarte?</label>
-            <input {...register('description')} type="description" className="form-control" />
+            <input name='description' type="description" className="form-control" />
           </div>
           <button type="submit" className="btn btn-dark submit">Enviar</button>
         </form>
